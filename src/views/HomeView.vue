0edf5ef4c9c61650efa3d9ca00e98e4775761155
wux <wux@greatld.com>
2024-03-21 16:13:58 +0800

    feat: init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import {onMounted, ref} from 'vue'
+import * as Cesium from 'cesium'
+
+onMounted(() => {
+  const position = Cesium.Cartesian3.fromDegrees(-75.59777, 40.03883, 10000.0);  
+  const heading = Cesium.Math.toRadians(0.0); // 朝向的方位角（以弧度为单位）  
+  const pitch = Cesium.Math.toRadians(0.0);
+  const roll = 0.0
+  const orientation = {  
+    heading : heading,  
+    pitch : pitch,  
+    roll : roll  
+  };  
+  const viewer = new Cesium.Viewer('cesiumContainer', {
+    //这里是配置项
+  })
+  viewer.camera.setView({  
+    destination : position,
+    orientation : orientation  
+  });
+})
+</script>
+
+<template>
+  <div id="cesiumContainer" class="cesium-container"></div>
+</template>
+
+<style scoped>
+#cesiumContainer {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import VueDevTools from 'vite-plugin-vue-devtools'
+import cesium from 'vite-plugin-cesium';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    VueDevTools(),
+    cesium()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
