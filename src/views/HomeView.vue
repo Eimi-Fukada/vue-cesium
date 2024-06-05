44e0b82b3cc8e87b34206c01018fb369634c4d64
grace <1745282879@qq.com>
2024-06-05 10:31:11 +0800

    feat: 花式打印console工具库

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import * as Cesium from 'cesium'
+import { prettyLog } from '@/utils/prettylog';
 onMounted(() => {
   const defaultToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4MGQ0YTBmOS0zNTY2LTQ5ZmUtODY2My1jMTc1OTIzMjU0MDAiLCJpZCI6MTk1MDUwLCJpYXQiOjE3MDc2NTA1Mjh9.AG76qYPGsQvT0kbsRba0vrA8Hm3KICp_3VYk0kq3msQ'
   Cesium.Ion.defaultAccessToken = defaultToken
@@ -20,6 +21,13 @@ onMounted(() => {
     vrButton: true
   })
 })
+
+const log = prettyLog();
+log.success('成功测试')
+log.error('错误测试')
+log.info('信息测试')
+log.warning('警告测试')
+log.picture('https://nimg.ws.126.net/?url=http%3A%2F%2Fdingyue.ws.126.net%2F2024%2F0514%2Fd0ea93ebj00sdgx56001xd200u000gtg00hz00a2.jpg&thumbnail=660x2147483647&quality=80&type=jpg')
 </script>
 
 <template>
